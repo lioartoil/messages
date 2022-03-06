2d7d01808a3d9070a70d43e50951a5e15b061f06
lioartoil <a.chanakant@gmail.com>
2022-03-06 17:03:58 +0700

    Add messages' routes

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Post } from '@nestjs/common';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+  @Get()
+  listMessages() {
+    return `['Message', 'Message']`;
+  }
+
+  @Post()
+  createMessage() {
+    return { content: 'message' };
+  }
+
+  @Get(':id')
+  getMessage(@Param('id') id: string) {
+    return `${id}'s message`;
+  }
+}
