448756ae644d3e0ead2ee0a7cbd3cdaddc4c73b9
lioartoil <a.chanakant@gmail.com>
2022-03-11 21:37:22 +0700

    Add validator

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,5 +1,7 @@
 import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 
+import { CreateMessageDto } from './dtos/create-message.dto';
+
 @Controller('messages')
 export class MessagesController {
   @Get()
@@ -8,7 +10,7 @@ export class MessagesController {
   }
 
   @Post()
-  createMessage(@Body() body: unknown) {
+  createMessage(@Body() body: CreateMessageDto) {
     return body;
   }
 
